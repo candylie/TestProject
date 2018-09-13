0a32cc55fd30181e4a7aa3cd83e764242083c86a
张科 <zhangke@thinkive.com>
2018-09-13 15:44:29 +0800

    初始化提交

---
+++ tabletmodule/src/main/java/test/zk/com/tabletmodule/TableModuleActivity.kt
@@ -0,0 +1,12 @@
+package test.zk.com.tabletmodule
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TableModuleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_table_module)
+    }
+}
